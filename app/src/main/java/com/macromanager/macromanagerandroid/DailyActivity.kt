7cb7e283147b9f38a06983592e5f1bd3ee0c47f6
mcuin <mykcuin@gmail.com>
2017-07-06 16:05:45 -0600

    Created a settings page with settings that would not need to be changed too often.

--- app/src/main/java/com/macromanager/macromanagerandroid/DailyActivity.kt
+++ app/src/main/java/com/macromanager/macromanagerandroid/DailyActivity.kt
@@ -1,12 +1,41 @@
 package com.macromanager.macromanagerandroid
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
 
 class DailyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.action_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item!!.itemId) {
+
+            R.id.action_settings -> {
+                val settingsIntent = Intent(this, SettingsActivity::class.java)
+
+                startActivity(settingsIntent)
+
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
